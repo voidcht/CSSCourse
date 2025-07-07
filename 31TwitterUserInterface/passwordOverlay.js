f9ae3397125e52e884dbcf3ccd4cdfdfb075c4d6
voidcht <chathurangilansakara@gmail.com>
2025-07-07 14:39:43 +0530

    password overlay completed

--- 31TwitterUserInterface/passwordOverlay.js
+++ 31TwitterUserInterface/passwordOverlay.js
@@ -2,7 +2,7 @@ const passwordField = document.getElementById("password");
 const togglePassword = document.querySelector(".password-toggle-icon i");
 
 togglePassword.addEventListener("click", function() {
-  if (passwordField.type === "password"){
+  if (passwordField.type === "password" && passwordField.value.length > 0){
     passwordField.type= "text";
     togglePassword.classList.remove("fa-eye");
     togglePassword.classList.add("fa-eye-slash");
